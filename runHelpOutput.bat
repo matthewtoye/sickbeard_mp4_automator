3cad3a75224cbc48288b81309a68acca531dd84d
Collision <s.alexlowery@gmail.com>
2017-01-07 01:28:19 -0500

    Add more help output to diff in the future.

--- runHelpOutput.bat
+++ runHelpOutput.bat
@@ -1,1 +1,10 @@
 ffmpeg -h full > ffmpegHelpOutput.txt
+ffmpeg -encoders >> ffmpegHelpOutput.txt
+ffmpeg -decoders >> ffmpegHelpOutput.txt
+ffmpeg -codecs >> ffmpegHelpOutput.txt
+ffmpeg -pix_fmts >> ffmpegHelpOutput.txt
+ffmpeg -formats >> ffmpegHelpOutput.txt
+ffmpeg -muxers >> ffmpegHelpOutput.txt
+ffmpeg -demuxers >> ffmpegHelpOutput.txt
+ffmpeg -hwaccels >> ffmpegHelpOutput.txt
+ffmpeg -filters >> ffmpegHelpOutput.txt
